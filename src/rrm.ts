c8db5ee6ae792979b39b8c28752f7d0e005a1cb0
pfg <pfg@pfg.pw>
2020-07-23 16:55:34 -0700

    rest@rt command

--- src/rrm.ts
+++ src/rrm.ts
@@ -1,7 +1,10 @@
 import * as discord from "discord.js";
 import * as secret from "./secret.json";
+import { promises as fs } from "fs";
 
-const client = new discord.Client();
+const client = new discord.Client({
+    partials: ["MESSAGE", "CHANNEL", "REACTION"]
+});
 client.login(secret.token);
 
 const channelIDs = {
@@ -16,10 +19,45 @@ const msgopts: discord.MessageOptions = {
     allowedMentions: { parse: [], roles: [], users: [] }
 };
 
+client.on("ready", async () => {
+    console.log("started");
+    try {
+        let [rschanid, rsmsgid] = (
+            await fs.readFile("__restarting", "utf-8")
+        ).split("|");
+        await fs.unlink("__restarting");
+        let chan = (await client.channels.fetch(
+            rschanid
+        )) as discord.TextChannel;
+        let msg = await chan.messages.fetch(rsmsgid);
+        await chan.send("yay im back");
+        await msg.delete();
+    } catch {}
+});
+
 client.on("message", async msg => {
+    if (msg.partial) await msg.fetch();
     if (msg.channel.type === "dm") {
         if (!msg.author.bot) await msg.reply("dont pm me :(");
         return;
+    }
+    if (msg.content.includes(client.user!.id) && !msg.author.bot) {
+        if (msg.content.includes("restart")) {
+            if (msg.author.id === "341076015663153153") {
+                let msgsnt = await msg.reply(
+                    "ok. ill be back in a moment <a:loading:721573242686668861>"
+                );
+                await fs.writeFile(
+                    "__restarting",
+                    msg.channel + "|" + msgsnt.id
+                );
+                process.exit(0);
+            } else {
+                await msg.reply("no you dont have control over me >:[");
+            }
+            return;
+        }
+        await msg.reply("dont at me im busy");
     }
     if (msg.channel.parent?.id !== channelIDs.activeTicketsCategory) return; // wrong channel nope
     await getChannel(channelIDs.transcripts).send(
@@ -34,6 +72,10 @@ client.on("message", async msg => {
             msgopts
         );
     }
+});
+
+client.on("messageReactionAdd", async (rxn, usr) => {
+    console.log(rxn, usr);
 });
 
 // client.on("");
